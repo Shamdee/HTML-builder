77d2a6a187642ec5a154302bf850ddb8db098f6b
Shamdee <shamdee@gmail.com>
2022-11-05 23:13:58 +0200

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  if (err) throw err;
+  files.forEach(item => {
+    fs.copyFile(path.join(__dirname, 'files', item), path.join(__dirname, 'files-copy', item), err => {
+      if (err) throw err;
+    });
+  });
+});
